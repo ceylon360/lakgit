84e8bb1f3d73b72590c15c9137175be91f2dc9f3
sudhara429 <sudhara.dananjaya@gmail.com>
2015-09-09 23:15:34 +0530

    module for delivery details

---
+++ includes/languages/english/modules/boxes/bm_information2.php
@@ -0,0 +1,20 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_BOXES_INFORMATION2_TITLE', 'Information2');
+  define('MODULE_BOXES_INFORMATION2_DESCRIPTION', 'Show information page links');
+  define('MODULE_BOXES_INFORMATION2_BOX_TITLE', 'Information2');
+  define('MODULE_BOXES_INFORMATION2_BOX_PRIVACY', 'Privacy Notice');
+  define('MODULE_BOXES_INFORMATION2_BOX_CONDITIONS', 'Conditions of Use');
+  define('MODULE_BOXES_INFORMATION2_BOX_SHIPPING', 'Shipping &amp; Returns');
+  define('MODULE_BOXES_INFORMATION2_BOX_CONTACT', 'Contact Us');
+?>
