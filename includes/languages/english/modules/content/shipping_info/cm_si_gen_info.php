fe1d411a60621be4574f2157290bc6ad301ecb24
sudhara429 <sudhara.dananjaya@gmail.com>
2015-09-10 16:47:02 +0530

    shipping information modules

---
+++ includes/languages/english/modules/content/shipping_info/cm_si_gen_info.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2014 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_SHIPPING_INFO_GEN_INFO_TITLE', 'Shipping infomation');
+  define('MODULE_CONTENT_SHIPPING_INFO_GEN_INFO_DESCRIPTION', 'Show shipping information.');
+  
+  define('MODULE_CONTENT_SHIPPING_INFO_GEN_INFO_TEXT_RATED', 'Rated %s by <cite title="%s">%s</cite>');
+
--- includes/modules/boxes/templates/information2.php
+++ includes/modules/boxes/templates/information2.php
@@ -1,6 +1,8 @@
-<div class="panel panel-default">
-  <div class="panel-heading"><?php echo MODULE_BOXES_INFORMATION2_BOX_TITLE; ?></div>
+<!--<div class="panel panel-default">
+  <div class="panel-heading"><?php// echo MODULE_BOXES_INFORMATION2_BOX_TITLE; ?></div>
   <div class="panel-body">
-    <img src="images/call.jpg" alt="" width="130" class="">
+     
   </div>
-</div>
+</div>-->
+
+<?php echo $shopping_data; ?>
