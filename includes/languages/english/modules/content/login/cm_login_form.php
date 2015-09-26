6bf5338b024e5312809a59952b8ba59f003a0fbe
sudhara429 <sudhara.dananjaya@gmail.com>
2015-09-26 12:02:59 +0530

    ver 1

--- includes/languages/english/modules/content/login/cm_login_form.php
+++ includes/languages/english/modules/content/login/cm_login_form.php
@@ -13,7 +13,7 @@
   define('MODULE_CONTENT_LOGIN_FORM_TITLE', 'Login Form');
   define('MODULE_CONTENT_LOGIN_FORM_DESCRIPTION', 'Show a login form on the login page');
 
-  define('MODULE_CONTENT_LOGIN_HEADING_RETURNING_CUSTOMER', 'Returning Customer');
+  define('MODULE_CONTENT_LOGIN_HEADING_RETURNING_CUSTOMER', 'Lakwimana Member');
   define('MODULE_CONTENT_LOGIN_TEXT_RETURNING_CUSTOMER', 'I am a returning customer.');
   define('MODULE_CONTENT_LOGIN_TEXT_PASSWORD_FORGOTTEN', 'Password forgotten? Click here.');
 
--- includes/languages/english/shipping.php
+++ includes/languages/english/shipping.php
@@ -13,5 +13,38 @@
 define('NAVBAR_TITLE', 'Shipping &amp; Returns');
 define('HEADING_TITLE', 'Shipping &amp; Returns');
 
-define('TEXT_INFORMATION', 'Put here your Shipping &amp; Returns information.');
+define('TEXT_INFORMATION', '<strong>Delivery charges: </strong>
+</br></br>
+We do deliveries for every order free of charges in Colombo district, without considering the order amount.
+</br></br>
+
+<strong>Prior Notice  </strong>
+</br></br>
+We require you to place the order at least 24 hour prior notice but we try our best to proceed with orders which are not given the 24 hour prior notice 
+</br></br>
+<strong>Order Message</strong>
+</br></br>
+We give an opportunity to add a message with your order, we will attach it  
+To a Lakwimana card and will be handed over to the recipient
+</br></br>
+
+<strong>Delivery Conformation</strong>
+</br></br>
+We assure you 100% to deliver your order on time without failure, and we will send you a delivery conformation as soon as we deliver your order to the given mail account
+</br></br>
+<strong>Surprise orders </strong>
+</br></br>
+Thought you are requested to enter the recipient’s phone number, if it is a surprise order we do not contact recipient prior to the delivery but if the address is difficult to be find we will call the recipient to get direction but we request our users to kindly note that Lakwimana do not undertake surprise order for food items such as dinners& lunches,
+</br></br>
+
+<strong>Our holidays</strong>
+</br></br>
+We deliver on week ends & all the public holidays except Independence Day & sinhala & Tamil New year day
+</br></br>
+<strong>Special Instructions</strong>
+</br></br>
+You are able to enter special instructions about your order, which we will read & follow
+</br></br>
+
+*We can not be held responsible for incorrect information supplied by the customer');
 ?>
