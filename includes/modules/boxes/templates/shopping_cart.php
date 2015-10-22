13c94d62ffabf96ea6fe9078b55c67769cdc5d7b
sudhara429 <sudhara.dananjaya@gmail.com>
2015-10-22 20:59:18 +0530

    cartbox head

--- includes/modules/boxes/templates/shopping_cart.php
+++ includes/modules/boxes/templates/shopping_cart.php
@@ -1,5 +1,5 @@
 <div class="panel panel-default bg-gray">
-  <div class="panel-heading"><a href="<?php echo tep_href_link('shopping_cart.php'); ?>"><?php echo MODULE_BOXES_SHOPPING_CART_BOX_TITLE; ?></a></div>
+<div class="panel-heading cartbox"><a href="<?php echo tep_href_link('shopping_cart.php'); ?>"><?php echo '<span style="font-size: 19px;"><i class="fa fa-shopping-cart"></i></span>   '.MODULE_BOXES_SHOPPING_CART_BOX_TITLE; ?></a></div>
   <div class="panel-body">
     <div class="shoppingCartList">
       <?php echo $cart_contents_string; ?>
